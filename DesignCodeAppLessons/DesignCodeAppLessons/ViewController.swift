01cc98a6afc3fd10eb26d4137663cac1cd372ea9
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-07-17 11:18:12 +0300

    initial commit

---
+++ DesignCodeAppLessons/DesignCodeAppLessons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignCodeAppLessons
+//
+//  Created by Mykhailo Bondarenko on 7/17/19.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

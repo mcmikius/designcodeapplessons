0a851b7cf5adb4611cc55c57000406db64fb1473
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-07-17 17:43:39 +0300

    Design in Playground Design and code UI in Swift Playground

---
+++ DesignCode-Design.playground/Contents.swift
@@ -0,0 +1,50 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let titleLabel = UILabel()
+        titleLabel.frame = CGRect(x: 16, y: 16, width: 272, height: 38)
+        titleLabel.text = "Learn Swift 4"
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        
+        let captionLabel = UILabel()
+        captionLabel.frame = CGRect(x: 16, y: 204, width: 272, height: 40)
+        captionLabel.text = "Design directly in Playground"
+        captionLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        let coverImageView = UIImageView()
+        coverImageView.frame = CGRect(x: 0, y: 0, width: 300, height: 250)
+        coverImageView.contentMode = .scaleAspectFill
+        coverImageView.image = #imageLiteral(resourceName: "Cover.jpg")
+        coverImageView.clipsToBounds = true
+        coverImageView.layer.cornerRadius = 14
+        
+        let backgroundImageView = UIImageView()
+        backgroundImageView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+        backgroundImageView.image = #imageLiteral(resourceName: "Chapters Screen@2x.png")
+        
+        let cardView = UIView()
+        cardView.frame = CGRect(x: 20, y: 255, width: 300, height: 250)
+        cardView.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+        cardView.layer.cornerRadius = 14
+        cardView.layer.shadowOpacity = 0.25
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        cardView.layer.shadowRadius = 10
+        
+        view.addSubview(backgroundImageView)
+        view.addSubview(cardView)
+        cardView.addSubview(coverImageView)
+        cardView.addSubview(titleLabel)
+        cardView.addSubview(captionLabel)
+        self.view = view
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()

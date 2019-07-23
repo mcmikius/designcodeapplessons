cdae6e32647085b8663dabb63b09b2eb4694a75d
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-07-23 19:04:37 +0300

    Importing MKRingProgressView

--- DesignCodeAppLessons/Pods/Target Support Files/Pods-DesignCodeAppLessons/Pods-DesignCodeAppLessons-frameworks.sh
+++ DesignCodeAppLessons/Pods/Target Support Files/Pods-DesignCodeAppLessons/Pods-DesignCodeAppLessons-frameworks.sh
@@ -161,10 +161,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MKRingProgressView/MKRingProgressView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Spring/Spring.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TPKeyboardAvoiding/TPKeyboardAvoiding.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MKRingProgressView/MKRingProgressView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Spring/Spring.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TPKeyboardAvoiding/TPKeyboardAvoiding.framework"
 fi
